431a9d4270237c36fe21ea7246f6f7afb96889d1
Abdushukur <abduhakimovabdushukur@gmail.com>
2022-10-06 04:09:20 +0500

    juft-toq

---
+++ media.js
@@ -0,0 +1,11 @@
+let answer = +prompt('son kiriting')
+if (isNaN(answer) || answer == '') {
+    alert('bu son emas, iltimos son kiriting')
+}
+let useranswer = +prompt('son kiriting')
+a = Number(useranswer)
+if(useranswer % 2 == 0){
+    alert(' bu juft son')
+}else{
+    alert('bu toq son')
+}
